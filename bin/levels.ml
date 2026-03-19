51c6b12a0699bb2d34b9cf0fc86da14f388c2e37
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2026-03-19 22:39:10 +0100

    feat: level selector + hover preview,

--- bin/levels.ml
+++ bin/levels.ml
@@ -1,15 +1,47 @@
-let level1 = [|
-    "o o o o o o o o";
-    "o o o o o o o o";
-    "o o o o x o o o";
-    "o o o o x o o o";
-    "o o x o x o o o";
-    "o x x o o x o o";
-    "o o x o o o o o";
-    "o o o o o o o o";
-|] (* 3 *)
+type range = Range of int * int
 
-let level2 = [|
+type level = {
+  grid: string array option;
+  par:  range option;
+}
+
+let make grid par = { grid = Some grid; par }
+let empty = { grid = None; par = None }
+
+let level1 = make [|
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "o o o x o o o o";
+    "o o x x x o o o";
+    "o o o x o o o o";
+    "o o o o o o o o";
+    "o o o o o o o o";
+|] (Some (Range (6, 6)))
+
+let level2 = make [|
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "o o x x x o o o";
+    "o o x x x o o o";
+    "o o x x x o o o";
+    "o o o o o o o o";
+    "o o o o o o o o";
+|] (Some (Range (6, 6)))
+
+let level3 = make [|
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "o o o o x o o o";
+    "o o o o x x o o";
+    "o o x x o o o o";
+    "o o o x o o o o";
+    "o o o o o o o o";
+    "o o o o o o o o";
+|] (Some (Range (6, 6)))
+
+let level12 = make [|
     "o o o o x o x x";
     "x x o x x x x x";
     "o x x x x o x x";
@@ -18,4 +50,32 @@ let level2 = [|
     "x x x x x x o x";
     "o o o o x o o x";
     "o o o o o o o x";
-|] (* 6 *)
+|] (Some (Range (6, 6)))
+
+let level4 = make [|
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "o o o o x o o o";
+    "o o o o x o o o";
+    "o o x o x o o o";
+    "o x x o o x o o";
+    "o o x o o o o o";
+    "o o o o o o o o";
+|] (Some (Range (3, 6)))
+
+let level15 = make [|
+    "o o x o o x o o";
+    "o o x o o x o o";
+    "x x x o o x x x";
+    "o o o o o o o o";
+    "o o o o o o o o";
+    "x x x o o x x x";
+    "o o x o o x o o";
+    "o o x o o x o o";
+|] (Some (Range (6, 12)))
+
+let all = [|
+  level1; level2; level3; level4; empty; empty;
+  empty; empty; empty; empty; empty; empty;
+  empty; empty; empty; empty; level15; empty;
+|]
--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 open Tsdl
 open Tsdl_ttf
 
+type shape = Dot | Dot4 | Plus | LPlus
+
 (* ui *)
 
 let margin = 2
@@ -33,9 +35,21 @@ let btn_w = (rpanel_w - 2 * rpad - btn_gap) / 2
 let btn_h = 36
 let btn_y0 = rpanel_y + grid_h - rpad - (3 * btn_h + 2 * btn_gap)
 
+let lvl_cols = 6
+let lvl_rows = 3
+let lvl_gap = rpad
+let lvl_btn_w = (window_w - 2 * margin - 2 * rpad - (lvl_cols - 1) * lvl_gap) / lvl_cols
+let lvl_btn_h = (bpanel_h - 2 * rpad - (4 - 1) * lvl_gap) / 4
+let lvl_total_w = lvl_cols * lvl_btn_w + (lvl_cols - 1) * lvl_gap
+let lvl_x0 = bpanel_x + (window_w - 2 * margin - lvl_total_w) / 2
+let lvl_y0 = bpanel_y + rpad
+
 let state = Array.make (grid_size * grid_size) false
-let selected = ref (-1, -1)
+let hover = Array.make (grid_size * grid_size) false
+
+let selected = ref Dot
 let steps = ref 0
+let current_level = ref 0
 
 let xytoi x y = y * grid_size + x
 let itoxy i = i mod grid_size, i/grid_size
@@ -54,8 +68,8 @@ let btn_colors = [|
     (255, 255, 255);  (* reset *)
 |]
 
-let draw_text renderer font text cx cy =
-    let color = Sdl.Color.create ~r:255 ~g:255 ~b:255 ~a:255 in
+let draw_text renderer font text cx cy r g b =
+    let color = Sdl.Color.create ~r ~g ~b ~a:255 in
     match Ttf.render_text_solid font text color with
     | Error _ -> ()
     | Ok surface ->
@@ -70,7 +84,7 @@ let draw_text renderer font text cx cy =
                 ignore (Sdl.render_copy renderer texture ~dst));
             Sdl.destroy_texture texture
 
-let draw_dolm renderer font =
+let draw_dolm renderer font small_font =
     (* background *)
     ignore (Sdl.set_render_draw_color renderer 0 0 0 255);
     ignore (Sdl.render_clear renderer);
@@ -83,6 +97,7 @@ let draw_dolm renderer font =
         ignore (Sdl.render_draw_line renderer grid_x y (grid_x + grid_w - 1) y);
     done;
     (* right panel buttons *)
+    let shapes = [|Dot; Dot4; Plus; LPlus|] in
     for row = 0 to 1 do
         for col = 0 to 1 do
             let idx = row * 2 + col in
@@ -90,12 +105,16 @@ let draw_dolm renderer font =
             let bx = preview_x + col * (btn_w + btn_gap) in
             let by = btn_y0 + row * (btn_h + btn_gap) in
             ignore (Sdl.set_render_draw_color renderer r g b 255);
-            draw_filled_rect renderer bx by btn_w btn_h
+            if !selected = shapes.(idx) then
+                draw_filled_rect renderer bx by btn_w btn_h
+            else
+                draw_rect_border renderer bx by btn_w btn_h
         done
     done;
-    let (r, g, b) = btn_colors.(4) in
-    ignore (Sdl.set_render_draw_color renderer r g b 255);
-    draw_filled_rect renderer preview_x (btn_y0 + 2 * (btn_h + btn_gap)) (btn_w * 2 + btn_gap) btn_h;
+    let reset_y = btn_y0 + 2 * (btn_h + btn_gap) in
+    ignore (Sdl.set_render_draw_color renderer 255 255 255 255);
+    draw_filled_rect renderer preview_x reset_y (btn_w * 2 + btn_gap) btn_h;
+    draw_text renderer small_font "RESET" (preview_x + (btn_w * 2 + btn_gap) / 2) (reset_y + btn_h / 2) 0 0 0;
     (* grid state *)
     ignore (Sdl.set_render_draw_color renderer 255 255 255 255);
     for i = 0 to Array.length state - 1 do
@@ -103,9 +122,13 @@ let draw_dolm renderer font =
             let (x, y) = itoxy i in
             ignore (Sdl.render_fill_rect renderer (Some (Sdl.Rect.create ~x:(grid_x + x*tile_size+1) ~y:(grid_y + y*tile_size+1) ~w:(tile_size-1) ~h:(tile_size-1))));
     done;
-    (* selected *)
+    (* grid hover *)
     ignore (Sdl.set_render_draw_color renderer 128 128 128 128);
-    ignore (let x, y = !selected in Sdl.render_fill_rect renderer (Some (Sdl.Rect.create ~x:(grid_x + x*tile_size+1) ~y:(grid_y + y*tile_size+1) ~w:(tile_size-1) ~h:(tile_size-1))));
+    for i = 0 to Array.length hover - 1 do
+        if hover.(i) then
+            let (x, y) = itoxy i in
+            ignore (Sdl.render_fill_rect renderer (Some (Sdl.Rect.create ~x:(grid_x + x*tile_size+1) ~y:(grid_y + y*tile_size+1) ~w:(tile_size-1) ~h:(tile_size-1))));
+    done;
     (* panel borders *)
     ignore (Sdl.set_render_draw_color renderer 255 255 255 255);
     draw_rect_border renderer grid_x grid_y grid_w grid_h;
@@ -113,31 +136,67 @@ let draw_dolm renderer font =
     draw_rect_border renderer bpanel_x bpanel_y (window_w - 2 * margin) bpanel_h;
     draw_rect_border renderer preview_x preview_y preview_size preview_size;
     (* steps counter *)
-    draw_text renderer font (string_of_int !steps) (preview_x + preview_size / 2) (preview_y + preview_size / 2)
+    draw_text renderer font (string_of_int !steps) (preview_x + preview_size / 2) (preview_y + preview_size / 2) 255 255 255;
+    (* level selector *)
+    for i = 0 to Array.length Levels.all - 1 do
+        let row = i / lvl_cols in
+        let col = i mod lvl_cols in
+        let bx = lvl_x0 + col * (lvl_btn_w + lvl_gap) in
+        let by = lvl_y0 + row * (lvl_btn_h + lvl_gap) in
+        let is_current = i = !current_level in
+        if is_current then begin
+            ignore (Sdl.set_render_draw_color renderer 255 255 255 255);
+            draw_filled_rect renderer bx by lvl_btn_w lvl_btn_h
+        end else begin
+            ignore (Sdl.set_render_draw_color renderer 255 255 255 255);
+            draw_rect_border renderer bx by lvl_btn_w lvl_btn_h
+        end
+    done
 
 
 (* logic *)
 
-type shape = Dot | Dot4 | Plus | LPlus
+let load_level (lvl : Levels.level) =
+    match lvl.grid with
+    | None -> Array.fill state 0 (Array.length state) false
+    | Some grid ->
+        Array.iteri (fun y row ->
+            for x = 0 to grid_size - 1 do
+                state.(xytoi x y) <- row.[x * 2] = 'x'
+            done
+        ) grid
 
-let load_level level =
-    Array.iteri (fun y row ->
-       	for x = 0 to grid_size - 1 do
-            state.(xytoi x y) <- row.[x * 2] = 'x';
-        done
-    ) level
-
-let toggle x y =
+let toggle_state x y =
 	if x >= 0 && x < grid_size && y >= 0 && y < grid_size then state.(xytoi x y) <- not state.(xytoi x y)
 
 let spell x y s =
     if x >= 0 && x < grid_size && y >= 0 && y < grid_size then begin
         (match s with
-        | Dot  -> toggle x y
-        | Dot4 -> toggle (x+1) (y+1); toggle (x+1) (y-1); toggle (x-1) (y+1); toggle (x-1) (y-1);
-        | Plus -> toggle x y; toggle (x-1) y; toggle (x+1) y; toggle x (y-1); toggle x (y+1)
-        | LPlus  -> for cy = 0 to grid_size - 1 do toggle x cy done;for cx = 0 to grid_size - 1 do toggle cx y done; toggle x y);
+        | Dot  -> toggle_state x y
+        | Dot4 -> toggle_state (x+1) (y+1); toggle_state (x+1) (y-1); toggle_state (x-1) (y+1); toggle_state (x-1) (y-1);
+        | Plus -> toggle_state x y; toggle_state (x-1) y; toggle_state (x+1) y; toggle_state x (y-1); toggle_state x (y+1)
+        | LPlus  -> for cy = 0 to grid_size - 1 do toggle_state x cy done;for cx = 0 to grid_size - 1 do toggle_state cx y done; toggle_state x y);
         incr steps
+    end
+
+let clear_hover h =
+    for y = 0 to grid_size - 1 do
+       	for x = 0 to grid_size - 1 do
+            h.(xytoi x y) <- false;
+        done
+    done
+
+let toggle_hover x y =
+    if x >= 0 && x < grid_size && y >= 0 && y < grid_size then hover.(xytoi x y) <- true
+
+let do_hover x y s =
+    if x >= 0 && x < grid_size && y >= 0 && y < grid_size then begin
+        clear_hover hover;
+        (match s with
+        | Dot  -> toggle_hover x y
+        | Dot4 -> toggle_hover (x+1) (y+1); toggle_hover (x+1) (y-1); toggle_hover (x-1) (y+1); toggle_hover (x-1) (y-1);
+        | Plus -> toggle_hover x y; toggle_hover (x-1) y; toggle_hover (x+1) y; toggle_hover x (y-1); toggle_hover x (y+1)
+        | LPlus  -> for cy = 0 to grid_size - 1 do toggle_hover x cy done;for cx = 0 to grid_size - 1 do toggle_hover cx y done; toggle_hover x y);
     end
 
 
@@ -145,9 +204,12 @@ let main () = match Sdl.init Sdl.Init.(video + events) with
 | Error (`Msg e) -> Sdl.log "Init error: %s" e; 1
 | Ok () ->
     ignore (Ttf.init ());
-    match Ttf.open_font "assets/font/Micro5-Regular.ttf" 48 with
+    match Ttf.open_font "assets/font/Micro5-Regular.ttf" 72 with
     | Error (`Msg e) -> Sdl.log "Font error: %s" e; -1
     | Ok font ->
+    match Ttf.open_font "assets/font/Micro5-Regular.ttf" 36 with
+    | Error (`Msg e) -> Sdl.log "Font error: %s" e; -1
+    | Ok small_font ->
     match Sdl.create_window ~w:window_w ~h:window_h "Dolm" Sdl.Window.shown with
     | Error (`Msg e) -> Sdl.log "Create window error: %s" e; -1
     | Ok w ->
@@ -156,46 +218,67 @@ let main () = match Sdl.init Sdl.Init.(video + events) with
         | Ok renderer ->
         	ignore (Sdl.set_render_draw_blend_mode renderer Sdl.Blend.mode_blend);
             ignore (Sdl.render_set_logical_size renderer window_w window_h);
-            load_level Levels.level1;
+            load_level Levels.all.(0);
             let e = Sdl.Event.create () in
-            (* event loop *)
             let rec loop () =
                 while Sdl.poll_event (Some e) do
                     let typ = Sdl.Event.(get e typ) in
                     if typ = Sdl.Event.quit then raise Exit
-                    else if Sdl.Event.(get e typ) = Sdl.Event.mouse_button_down then begin
-                    let mx = Sdl.Event.(get e mouse_button_x) in
+                    else if typ = Sdl.Event.mouse_button_down then begin
+                        let mx = Sdl.Event.(get e mouse_button_x) in
                         let my = Sdl.Event.(get e mouse_button_y) in
                         if mx >= grid_x && mx < grid_x + grid_w && my >= grid_y && my < grid_y + grid_h then begin
                             let x = (mx - grid_x) / tile_size in
                             let y = (my - grid_y) / tile_size in
-                            selected := (x, y)
+                            spell x y !selected
                         end else begin
                             (* spell buttons *)
-                            let sx, sy = !selected in
                             for row = 0 to 1 do for col = 0 to 1 do
                                 let idx = row * 2 + col in
                                 let bx = preview_x + col * (btn_w + btn_gap) in
                                 let by = btn_y0 + row * (btn_h + btn_gap) in
                                 if mx >= bx && mx < bx + btn_w && my >= by && my < by + btn_h then
-                                    spell sx sy [|Dot; Dot4; Plus; LPlus|].(idx)
+                                    selected := [|Dot; Dot4; Plus; LPlus|].(idx)
                             done done;
                             (* reset button *)
                             let ry = btn_y0 + 2 * (btn_h + btn_gap) in
                             if mx >= preview_x && mx < preview_x + btn_w * 2 + btn_gap
                             && my >= ry && my < ry + btn_h then begin
-                                load_level Levels.level1; selected := (-1, -1); steps := 0
-                            end
+                                load_level Levels.all.(!current_level); steps := 0
+                            end;
+                            (* level buttons *)
+                            for i = 0 to Array.length Levels.all - 1 do
+                                let row = i / lvl_cols in
+                                let col = i mod lvl_cols in
+                                let bx = lvl_x0 + col * (lvl_btn_w + lvl_gap) in
+                                let by = lvl_y0 + row * (lvl_btn_h + lvl_gap) in
+                                if mx >= bx && mx < bx + lvl_btn_w && my >= by && my < by + lvl_btn_h then begin
+                                    current_level := i;
+                                    load_level Levels.all.(i);
+                                    steps := 0
+                                end
+                            done
                         end
                     end
+                    else if typ = Sdl.Event.mouse_motion then begin
+                        let mx = Sdl.Event.(get e mouse_motion_x) in
+                        let my = Sdl.Event.(get e mouse_motion_y) in
+                        if mx >= grid_x && mx < grid_x + grid_w && my >= grid_y && my < grid_y + grid_h then begin
+                            let x = (mx - grid_x) / tile_size in
+                            let y = (my - grid_y) / tile_size in
+                            do_hover x y !selected
+                        end else
+                            clear_hover hover
+                    end
                 done;
-                draw_dolm renderer font;
+                draw_dolm renderer font small_font;
                 Sdl.render_present renderer;
                 Sdl.delay 16l;
                 loop ()
             in
             (try loop () with Exit -> ());
             Ttf.close_font font;
+            Ttf.close_font small_font;
             Sdl.destroy_renderer renderer;
             Sdl.destroy_window w;
             Ttf.quit ();
