634a2ec2936d0b7189d515e45a313fcdce24a784
DCCXXV <110636189+DCCXXV@users.noreply.github.com>
2026-03-20 20:48:15 +0100

    feat: more levels

--- bin/levels.ml
+++ bin/levels.ml
@@ -21,13 +21,13 @@ let level1 = make ("I. The Star")
     "o o o o o o o o";
 |] (Range (1, 4))
 
-let level2 = make ("II. The Pillar")
+let level2 = make ("II. The Fish")
 [|
     "o o o o o o o o";
     "o o o o o o o o";
-    "o o o o o o o o";
+    "o o o x o x o o";
     "o o x x x o o o";
-    "o o x x x o o o";
+    "o o x o x x o o";
     "o o x x x o o o";
     "o o o o o o o o";
     "o o o o o o o o";
@@ -56,18 +56,6 @@ let level4 = make ("IV. The Scissors")
     "o o o x x x o x";
     "o o o o o o x o";
 |] (Range (4, 15))
-
-let level8 = make ("The Hermit")
-[|
-    "o o o o o o o o";
-    "o o o o o o o o";
-    "o o o o x o o o";
-    "o o o o x o o o";
-    "o o x o x o o o";
-    "o x x o o x o o";
-    "o o x o o o o o";
-    "o o o o o o o o";
-|] (Range (3, 6))
 
 let level5 = make ("V. The Loop")
 [|
@@ -105,7 +93,7 @@ let level7 = make ("VII. The Hanged Man")
     "o o o o o o o x";
 |] (Range (6, 6))
 
-let level13 = make ("XIII. The Flowers")
+let level8 = make ("VIII. The Flowers")
 [|
     "o x o o o o x o";
     "x o x o o x o x";
@@ -117,9 +105,69 @@ let level13 = make ("XIII. The Flowers")
     "o x o o o o x o";
 |] (Range (8, 12))
 
-let level16 = make ("XVI. The Wheel of Fortune")
+let level9 = make ("IX. The Hermit")
 [|
-	"x o x o o x o x";
+    "x o x o o o o o";
+    "o o o o o o x o";
+    "x x x o x x x x";
+    "x x x o x o x o";
+    "o x x o x o o o";
+    "o x x o o x o o";
+    "o o x o o x o o";
+    "o o o o x x x o";
+|] (Range (7, 25))
+
+let level10 = make ("X. Portals")
+[|
+    "o x o o o o x o";
+    "x x x o o x o x";
+    "o o o o o o o o";
+    "o x o o o o o o";
+    "o o o o o o x o";
+    "o o o o o o o o";
+    "x o x o o x x x";
+    "o x o o o o x o";
+|] (Range (10, 15))
+
+let level11 = make ("XI. Justice")
+[|
+    "o o o x x o o o";
+    "o x o x x o x o";
+    "o o x o o x o o";
+    "o x o x x o x o";
+    "x o o x x o o x";
+    "x x o x x o x x";
+    "x o o x x o o x";
+    "o o o x x o o o";
+|] (Range (8, 27))
+
+let level12 = make ("XII. Mitosis")
+[|
+    "x x o x x o o o";
+    "x x o x o o x o";
+    "o o o x x o o o";
+    "x x x o o x o x";
+    "x o x o o x x x";
+    "o o o x x o o o";
+    "o x o o x o x x";
+    "o o o x x o x x";
+|] (Range (10, 29))
+
+let level14 = make ("XII. The Trap")
+[|
+	"o o x o o x o o";
+	"o o o o x x x o";
+	"x o x o x x x x";
+	"o o o o x x x o";
+	"o x x x o o o o";
+	"x x x x o x o x";
+	"o x x x o o o o";
+	"o o x o o x o o";
+|] (Range (6, 27))
+
+let level16 = make ("XVI. #")
+[|
+    "x o x o o x o x";
     "o o x o o x o o";
     "x x x x x x x x";
     "o o x o o x o o";
@@ -139,10 +187,22 @@ let level17 = make ("XVII. The Cross")
     "x x x o o x x x";
     "o o x o o x o o";
     "o o x o o x o o";
-|] (Range (6, 12))
+|] (Range (16, 19))
+
+let level18 = make ("XVIII. The Wheel of Fortune")
+[|
+    "x x o x x o x x";
+    "x o x x x x o x";
+    "o x x o o x x o";
+    "x x o o x o x x";
+    "x x o x o o x x";
+    "o x x o o x x o";
+    "x o x x x x o x";
+    "x x o x x o x x";
+|] (Range (10, 31))
 
 let all = [|
     level1; level2; level3; level4; level5; level6;
-    level7; empty; empty; empty; empty; empty;
-    level13; empty; empty; level16; level17; empty;
+    level7; level8; level9; level10; level11; level12;
+    empty; level14; empty; level16; level17; level18;
 |]
